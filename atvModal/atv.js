830d904ae94426ea9945e63a30e5ee10169c7dca
Levy Cordeiro <108874182+lixoreciclavel@users.noreply.github.com>
2023-03-08 12:19:20 -0300

    Add files via upload

---
+++ atvModal/atv.js
@@ -0,0 +1,8 @@
+
+function toggleModal() {
+    document.getElementsByClassName('modal')[0].style.display = 'flex'
+}
+function offModal() {
+    document.getElementsByClassName('modal')[0].style.display = 'none'
+}
+console.log(offModal())
